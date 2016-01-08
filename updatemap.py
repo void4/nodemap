fbeb13027b81cdeec1aacdc75cad98b0ef2c6dff
void <void@119.io>
2016-01-08 01:02:40 +0100

    Cleaned up a bit, still terrible

--- updatemap.py
+++ updatemap.py
@@ -11,17 +11,16 @@ client = MongoClient()
 db = client["nodemap"]["ips"]
 
 for line in check_output("ipfs diag net", shell=True).split("\n"):
-  if line.strip().startswith("ID"):
-    peerid = line.strip().split(" ")[1]
+  line =  line.strip()
+  if line.startswith("ID"):
+    peerid = line.split(" ")[1]
     if peerid:
-      print("PEERID: ",peerid)
       try:
         output = ipfs.dht_findpeer(peerid, timeout=1)
         
         for ip in output.split("\n"):
           if ip.strip().startswith("/"):
             data = {"ip":ip.split("/")[2]}
-            print(data)
             db.replace_one(data, data, upsert=True)
       except:
         pass
@@ -29,15 +28,13 @@ for line in check_output("ipfs diag net", shell=True).split("\n"):
 ips = map(lambda x:x["ip"], db.find({}))
 
 s = ""
-yay = fail = 0
 for ip in ips:
   match = geolite2.lookup(ip.strip())
   if match:
     try:
       s += "[%f,%f]," % match.location
-      yay += 1
     except:
-      fail += 1
+      pass
 
 with open("index_template.html", "r") as f:
   html = f.read()
@@ -48,5 +45,4 @@ with open("index.html", "w+") as f:
   f.write(html)
 
 hsh = ipfs.add("index.html")["Hash"]
-print(hsh)
 ipfs.name_publish(hsh)
